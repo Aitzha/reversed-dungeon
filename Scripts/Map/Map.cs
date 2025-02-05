a0895d3f4b9f9ff1667130b45250b990c768e02e
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2025-02-05 20:17:56 +0500

    Fixed and improved map generation

--- Scripts/Map/Map.cs
+++ Scripts/Map/Map.cs
@@ -9,7 +9,9 @@ public partial class Map : Control
     [Export] private PackedScene enemyNodeScene;
     [Export] private PackedScene eliteEnemyNodeScene;
 
-    private int mapRows = 3;
+    // rows can only be odd number
+    private int mapRows = 5;
+    // col must be over or equal to 2
     private int mapCols = 6;
     private int borderX = 50;
     private int borderY = 60;
@@ -44,34 +46,40 @@ public partial class Map : Control
         {
             MapNode parent = q.Dequeue();
 
-            if (parent.col == mapCols - 2)
-            {
-                if (nodesDict[startNode.row].ContainsKey(parent.col + 1))
-                {
-                    parent.children.Add(nodesDict[startNode.row][parent.col + 1]);
-                }
-                else
-                {
-                    MapNode bossNode = (MapNode) bossNodeScene.Instantiate();
-                    AddChildNode(parent, startNode.row, bossNode);
-                }
-                continue;
-            }
-            
+            // Don't ask me how I calculated it. Just made this formula and it works but I can't prove it :P
+            int upperLimit = Math.Max(0, parent.col + 1 - (mapCols - 1 - (mapRows - 1) / 2));
+            int lowerLimit = mapRows - 1 - upperLimit;
             for (int i = -1; i < 2; i++)
             {
-                if (parent.row + i < 0 || parent.row + i > mapRows - 1)
+                if (parent.row + i < upperLimit || parent.row + i > lowerLimit)
                     continue;
-
+                
                 if (nodesDict[parent.row + i].ContainsKey(parent.col + 1))
                 {
                     parent.children.Add(nodesDict[parent.row + i][parent.col + 1]);
                 }
                 else
                 {
-                    MapNode child = (MapNode) enemyNodeScene.Instantiate();
-                    AddChildNode(parent, parent.row + i, child);
-                    q.Enqueue(child);
+                    // Instantiate node
+                    MapNode child;
+                    if (parent.col == mapCols - 2)
+                        child = (MapNode) bossNodeScene.Instantiate();
+                    else 
+                        child = (MapNode) enemyNodeScene.Instantiate();
+                    
+                    // Setup node
+                    child.col = parent.col + 1;
+                    child.row = parent.row + i;
+                    Vector2 offset = -child.Size / 2;
+                    child.Position = new Vector2(borderX + distX * child.col, borderY + distY * child.row) + offset;
+                    
+                    // Save child node
+                    nodesDict[child.row].Add(child.col, child);
+                    parent.children.Add(child);
+                    AddChild(child);
+                    
+                    if (child.col < mapCols - 1)
+                        q.Enqueue(child);
                 }
             }
         }
